878f9e960ce92863fc987251ae05c716701f2ee9
Ronnie95 <t_daron@yahoo.com>
2024-06-03 08:10:54 -0400

    input

---
+++ input.py
@@ -0,0 +1,3 @@
+# prompt for the user
+# input("What is your name?")
+print("Hello" + " " + input("what is your name?"))
--- print.py
+++ print.py
@@ -4,3 +4,12 @@ print("2. Knead the dough for 10 minutes.")
 print("3. Add 3g of Salt.")
 print("4. Leave to rise for 2 hours.")
 print("5. Bake at 200 degrees C for 30 minutes.")
+
+# modifier
+print('she said: "Hello" and then left.')
+
+# /n = new line
+print("Hello world!\nHello world!\nHello world!")
+
+# concate with space between strings
+print("Hello" + " " + "Ron")
