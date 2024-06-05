f954f7e3ae5228972b71851743b3913e9b6090f3
Ronnie95 <t_daron@yahoo.com>
2024-06-05 12:45:08 -0400

    updates

---
+++ Week1/randomz.py
@@ -0,0 +1,21 @@
+import random
+
+# random_int = random.randint(1,19)   #random whole #
+# print(random_int)
+
+# random_float = random.random()  #0.0000000000 to 0.99999999999
+# print(random_float)
+
+# random_float = random.random() * 5  #0.0000000000 to 5
+# print(random_float)
+
+# love_score = random.randint(0,100)
+# print(f"your love score is {love_score}")
+
+hot = random.randint(0,1)
+heads = 1
+tails = 0
+if hot == heads:
+    print("Heads ")
+else:
+    print("Tails")
---
+++ Week1/treasureIsland.py
@@ -0,0 +1,12 @@
+print("Welcome to Treasure Island \nYour mission is to find the treasure.")
+choice = input("your at a crossroad. Where do you want to go? Type 'left' or 'right'")
+# choices1 = 'left' or 'Left'
+# choices2 = 'right' or 'Right'
+if choice == 'left' or choice == 'Left':
+    choice2 = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.")
+else:
+    print("game over")
+input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.")
+
+# elif choice2 == 'swim':
+#     print("attacked by trout")
