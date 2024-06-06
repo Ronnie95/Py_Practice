5465a0edaa4b1a99ed5890e22b384415f0ae59cc
Ronnie95 <t_daron@yahoo.com>
2024-06-06 11:41:17 -0400

    updates

---
+++ Week1/list.py
@@ -0,0 +1,10 @@
+# states = ["New York", "Florida", "Ohio"]
+
+# # states[0] = "new york" Changes the data
+# # print(states[-1]) negative index pulls from the back 
+
+# # states.append("Maryland") adds item to the list 
+# print(states)
+
+print("Who's paying the bill?")
+names = ["Ronnie", "Ajak", "Mom", "Dad"]
---
+++ Week1/loops.py
@@ -0,0 +1,22 @@
+# sneakers = ["nike", "jordans", "adidas", "asics"]
+
+# for sneaker in sneakers:
+#     print(sneaker)
+
+
+# student_heights = [180, 124, 165, 173, 189, 169, 146]
+
+# for students in student_heights:
+#     print(students)
+
+# for number in range(1,100, 10):
+#     print(number)
+
+# total = 0
+# for number in range(1, 101):
+#     total += number
+#     print(total)
+
+# target =int(input())
+for target in range(0, 54, 2):
+    print(target)
