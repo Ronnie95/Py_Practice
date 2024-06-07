0820d857ce2d3fdc1083bd7c8f9ed3d1824a632d
Ronnie95 <t_daron@yahoo.com>
2024-06-07 12:31:24 -0400

    updates

--- Week1/hangMan.py
+++ Week1/hangMan.py
@@ -3,10 +3,13 @@ import random
 word_list = ["pickle","home","catostrophic","alphabet"]
 display = [""]
 chosen_word = random.choice(word_list)
+lives = 6
 for letter in chosen_word:
     display += "_"
 print(chosen_word)
 # print(display)
+
+
 
 end_of_game = False
 while not end_of_game:
@@ -17,6 +20,12 @@ while not end_of_game:
             display[position] = letter
 
     print(display)
+
+if guess not in chosen_word:
+    lives -= 1
+    if lives == 0:
+        end_of_game = True
+        print("you lose")
 
 if "_" not in display:
     end_of_game = True
