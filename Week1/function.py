4bd838a49eb646c6b9210bd10d80fde2be82ed6c
Ronnie95 <t_daron@yahoo.com>
2024-06-09 19:12:51 -0400

    updates

--- Week1/function.py
+++ Week1/function.py
@@ -13,3 +13,20 @@ def add(n1, n2):
 
 result = add(2,4)
 print(type(result))
+
+def greet(name):
+    print(f"Hello {name}")
+    print(f"how you doing {name}")
+
+greet("Ron")
+
+def greet_with(name, number): # positional arguement
+    print(f"Hello {name}")
+    print(f"Your number is {number}")
+
+greet_with("Ron", 12344334434)
+
+def fun(a=1, b =2 ,c =4): #Keyword argument
+    print(f"Hello {a},{b},{c}")
+
+fun()
