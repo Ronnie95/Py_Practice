db44442fc25931c69a57c4f9bd5239bf809c5286
Ronnie95 <t_daron@yahoo.com>
2024-07-01 10:59:09 -0400

    updated

--- Intermediate/oop.py
+++ Intermediate/oop.py
@@ -15,6 +15,6 @@ from prettytable import PrettyTable
 
 table = PrettyTable()
 table.add_column("Pokemon",["pikachu", "squirtle"])
-table.add_column("Pokemon",["pikachu", "squirtle"])
-table.align = "c"
+table.add_column("Type",["water", "fire"])
+table.align = "l"
 print(table)
