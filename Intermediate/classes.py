484fd0211867623d270083f797d9c929e1bce7af
Ronnie95 <t_daron@yahoo.com>
2024-07-26 11:02:54 -0400

    updates

--- Intermediate/classes.py
+++ Intermediate/classes.py
@@ -2,11 +2,21 @@ class User:
     def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
+        self.followers = 0
+        self.following = 0
 
+    def follow(self, user):
+        user.followers += 1
+        user.following += 1
 
 
 
 user_1 = User("002", "Ronnie")
-# user_1.id = "002"
-# user_1.username = "Ron"
-print(user_1.username)
+user_2 = User("003", "Ron")
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
+# print(user_1.username)>
