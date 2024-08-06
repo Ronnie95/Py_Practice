6e4a38d328ca8c7ba6038df3c6a0d28e15da9461
Ronnie95 <t_daron@yahoo.com>
2024-08-06 10:21:19 -0400

    updates

--- Intermediate/squirrel.py
+++ Intermediate/squirrel.py
@@ -2,4 +2,15 @@ import pandas as pd
 
 sq = pd.read_csv('./intermediate/squirrel.csv')
 
-print(sq)
+# print(sq)
+
+# print(sq.head(10)) prints the first # of  specified rows
+
+# print(sq.tail(5)) prints the last # of specified rows
+
+# print(sq.info())
+
+
+new_sq = sq.dropna()
+
+print(new_sq)
