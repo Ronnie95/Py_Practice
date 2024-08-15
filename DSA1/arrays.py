b50a81a5b5b818f037f05981d7e484cb17f29154
Ronnie95 <t_daron@yahoo.com>
2024-08-15 11:43:33 -0400

    updates

---
+++ DSA1/arrays.py
@@ -0,0 +1,15 @@
+array = [8,7,6,5,4,9]
+
+minVal = array[0]
+
+for min in array:
+    if min < minVal:
+        minVal = min
+
+
+print(f"lowest value:", minVal)
+
+# print(minVal)
+
+
+# print(array[2])
